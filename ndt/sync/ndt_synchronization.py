bcc297a4a10fcf5d65a3eec04dafb0be6f979b6a
claudio966 <claudiomatheus966@gmail.com>
2025-10-29 17:03:21 -0300

    chore: Refactoring the code to support multiple realizations

--- ndt/sync/ndt_synchronization.py
+++ ndt/sync/ndt_synchronization.py
@@ -13,33 +13,14 @@ from river import drift
 from std_train import get_mean_std_dict, train_and_evaluate, get_default_hyperparams
 import tensorflow as tf
 import std_delay_model
+import std_jitter_model
 
 
 model_version = 0
 async_running = False
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--topology", "-t", help="Type of topology to be used in the experiments.", 
-    type=str, required=True
-)
-parser.add_argument(
-    "--dir", "-d", help="Path to network traffic datasets.", 
-    type=str, required=True
-)
-parser.add_argument(
-    "--realization", "-r", help="Number of realization to be performed.", 
-    type=int, required=True
-)
-parser.add_argument(
-    "--sync", "-s", help="Enable twin synchronization.", 
-    action="store_true", required=False
-)
-
-args = parser.parse_args()
-
-def main_loop(realization: int):
+def main_loop(realization: int, target: str, data_dir, topology, sync):
     """
     Main loop traffic generation function
     """
@@ -49,56 +30,57 @@ def main_loop(realization: int):
     # used only to accelarate the simulation
     # when a retraining process is not running
     convey_time = 0
-    root_data_dir = args.dir # root directory for the database
-    if args.topology == "5g_crosshaul":
+    root_data_dir = data_dir # root directory for the database
+    if topology == "5g_crosshaul":
         dataset_name = "experiment_10"
         window_size = 6800
-    elif args.topology == "germany":
+    elif topology == "germany":
         window_size = 7000
         dataset_name = "experiment_20"
-    elif args.topology == "passion":
+    elif topology == "passion":
         dataset_name = "experiment_30"
         window_size = 6800
     else:
         raise ValueError("This is not a supported topology!")
 
     # Create the output directory
-    output_path_name = f"results/{args.topology}/"
+    output_path_name = f"results/{topology}/"
     if os.path.isfile(output_path_name):
         os.remove(output_path_name)
     else:
         pathlib.Path(output_path_name).mkdir(parents=True, exist_ok=True)
 
     # Create the weights directory
-    weights_path_name = f"weights/{args.topology}/"
+    weights_path_name = f"weights/{topology}/"
     if os.path.isfile(weights_path_name):
         os.remove(weights_path_name)
     else:
         pathlib.Path(weights_path_name).mkdir(parents=True, exist_ok=True)
 
     model_version = 0
-    training_data = [f"{root_data_dir}/{args.topology}/{dataset_name}0_cv",
-                    f"{root_data_dir}/{args.topology}/{dataset_name}1_cv",
-                    f"{root_data_dir}/{args.topology}/{dataset_name}2_cv",
-                    f"{root_data_dir}/{args.topology}/{dataset_name}4_cv"]
+    training_data = [f"{root_data_dir}/{topology}/{dataset_name}0_cv",
+                    f"{root_data_dir}/{topology}/{dataset_name}1_cv",
+                    f"{root_data_dir}/{topology}/{dataset_name}2_cv",
+                    f"{root_data_dir}/{topology}/{dataset_name}4_cv"]
 
-    model_weights_dir = f"weights/{args.topology}/model_version"
+    model_weights_dir = f"weights/{topology}/model_version"
 
-    if not os.path.isfile(f"{model_weights_dir}_{model_version}/final_weight.index"):
+    if not os.path.isfile(f"{model_weights_dir}_{model_version}/{target}_final_weight.index"):
         print("Training the initial model!!")
-        untrained_model = load_untrained_model()
+        untrained_model = load_untrained_model(target)
         initial_training_vtwin(training_data[model_version],
                                     untrained_model,
                                     model_weights_dir,
-                                    topology=args.topology,
-                                    realization=realization)
+                                    topology=topology,
+                                    realization=realization,
+                                    target=target)
 
-    stream_data_dir = [f"{root_data_dir}/{args.topology}/{dataset_name}0_cv/testing",
-                        f"{root_data_dir}/{args.topology}/{dataset_name}1_cv/testing",
-                        f"{root_data_dir}/{args.topology}/{dataset_name}2_cv/testing",
-                        f"{root_data_dir}/{args.topology}/{dataset_name}4_cv/testing"]
+    stream_data_dir = [f"{root_data_dir}/{topology}/{dataset_name}0_cv/testing",
+                        f"{root_data_dir}/{topology}/{dataset_name}1_cv/testing",
+                        f"{root_data_dir}/{topology}/{dataset_name}2_cv/testing",
+                        f"{root_data_dir}/{topology}/{dataset_name}4_cv/testing"]
 
-    weight_filename = f"{model_weights_dir}_{model_version}/final_weight.index"
+    weight_filename = f"{model_weights_dir}_{model_version}/{target}_final_weight.index"
     last_weight_id = os.path.getmtime(weight_filename)
     stream_data = tf.data.Dataset.load(f"{stream_data_dir[0]}", compression="GZIP")
 
@@ -108,7 +90,8 @@ def main_loop(realization: int):
         stream_data = stream_data.concatenate(new_data)
 
     trained_model = load_trained_model(training_data[model_version],
-                                        f"{model_weights_dir}_{model_version}/final_weight")
+                                        f"{model_weights_dir}_{model_version}/{target}_final_weight",
+                                        target)
     nmses, indexes, points = [], [], []
     flow_id = 0
 
@@ -120,42 +103,43 @@ def main_loop(realization: int):
     drift_detected = []
     for _, (sample_features, labels) in enumerate(stream_data):
         stream_data_sample = (sample_features, labels)
-        weight_filename = f"{model_weights_dir}_{model_version}/final_weight.index"
+        weight_filename = f"{model_weights_dir}_{model_version}/{target}_final_weight.index"
 
         # update virtual twin model
         if (os.path.isfile(weight_filename) and \
             os.path.getmtime(weight_filename) > last_weight_id \
-                                        and args.sync \
+                                        and sync \
                                         and not async_running):
-            print("New model")
+            print("\nNew model")
             model_updated.append(window_index)
             last_weight_id = os.path.getmtime(weight_filename)
             print(f"Model version {model_version} in production")
             trained_model = load_trained_model(training_data[model_version],
-                                    f"{model_weights_dir}_{model_version}/final_weight")
+                                    f"{model_weights_dir}_{model_version}/{target}_final_weight",
+                                    target)
 
         _, nmse = predicting_vtwin(trained_model, stream_data_sample)
         nmses.append(nmse)
-
         for flow_traffic in sample_features["flow_traffic"].numpy():
-            print(flow_id)
+            print("\r Analyzing flow ID: ", flow_id, end="", flush=True)
             kswin.update(flow_traffic)
-            if args.sync and kswin.drift_detected and not async_running:
+            if sync and kswin.drift_detected and not async_running:
                 drift_detected.append(window_index)
                 if model_version + 2 > len(training_data):
                     break
-                print("DRIFT detected")
+                print("\n\033[31m=> Drift detected\033[0m")
                 convey_time = 0.8
                 indexes.append(flow_id)
                 points.append(sample_features["flow_traffic"].numpy())
-                print("Retraing the model")
+                print("\033[32m=> Retraining the VTwin\033[0m")
                 async_running = True
                 model_version += 1
                 model_training = subprocess.Popen(["python3", "std_train.py",
                                 "--ds-train", f"{training_data[model_version]}",
                                 "--ckpt-path", f"{model_weights_dir}_{model_version}",
-                                "--topology", f"{args.topology}",
-                                "--realization", f"{realization}"])
+                                "--topology", f"{topology}",
+                                "--realization", f"{realization}",
+                                "--target", f"{target}"])
             flow_id += 1
             if flow_id > window_size - 200:
                 sleep(convey_time)
@@ -166,26 +150,37 @@ def main_loop(realization: int):
         window_index += 1
     if model_training is not None:
         model_training.kill()
-    print("Saving error Metrics")
-    np.savez(f"{output_path_name}/results_sync_{args.sync}_r_{realization}.npz",
+    print("=> Assessement finished!")
+    print("=> Saving error Metrics")
+    np.savez(f"{output_path_name}/results_sync_{target}_{sync}_r_{realization}.npz",
                                 np.array(nmses), drift_detected, model_updated)
 
-def load_untrained_model():
+def load_untrained_model(target):
     """
     function to load model without training weights
     """
-    model = std_delay_model.VirtualTwin
+    if target == "delay":
+        model = std_delay_model.VirtualTwin
+    elif target == "jitter":
+        model = std_jitter_model.VirtualTwin
+    else:
+        raise ValueError("Choose a proper QoS predictor model!")
 
     return model
 
 
-def load_trained_model(training_data, model_weights_file):
+def load_trained_model(training_data, model_weights_file, target):
     """
     function to load a trained GNN model
     """
-    model = std_delay_model.VirtualTwin()
 
-    model.load_weights(f"{model_weights_file}")
+    if target == "delay":
+        model = std_delay_model.VirtualTwin()
+    elif target == "jitter":
+        model = std_jitter_model.VirtualTwin()
+    else:
+        raise ValueError("Choose a proper QoS predictor model!")
+    model.load_weights(model_weights_file)
 
     # Compile the model
     model.compile(
@@ -222,7 +217,8 @@ def predicting_vtwin(trained_model, stream_data):
 def initial_training_vtwin(training_data, model,
                                 model_weights_dir,
                                 topology,
-                                realization):
+                                realization,
+                                target):
     """
     function to training GNN model
     """
@@ -232,15 +228,40 @@ def initial_training_vtwin(training_data, model,
     model_weights_dir = f"{model_weights_dir}_{model_version}/"
     train_and_evaluate(
         os.path.join(training_data),
-        model(),''
+        model(),
         **get_default_hyperparams(),
         ckpt_path=model_weights_dir,
         topology=topology,
-        realization=realization
+        realization=realization,
+        target=target
     )
 
 if __name__ == "__main__":
-    N_REALIZATIONS = args.realization
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--topology", "-t", help="Type of topology to be used in the experiments.", 
+        type=str, required=True
+    )
+    parser.add_argument(
+        "--dir", "-d", help="Path to network traffic datasets.", 
+        type=str, required=True
+    )
+    parser.add_argument(
+        "--target", "-g", help="QoS metric to be predicted.", 
+        type=str, required=True
+    )
+    parser.add_argument(
+        "--realization", "-r", help="Number of realization to be performed.", 
+        type=int, required=True
+    )
+    parser.add_argument(
+        "--sync", "-s", help="Enable twin synchronization.", 
+        action="store_true", required=False
+    )
 
-    for realization in range(N_REALIZATIONS):
-        main_loop(realization)
+    args = parser.parse_args()
+
+    for realization in range(args.realization):
+        print(f"\033[33m=> Realization #{realization}\033[0m")
+        main_loop(realization, args.target, args.dir,
+                                args.topology, args.sync)
