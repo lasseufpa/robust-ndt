6efb22a7c3d019ae6e55ae82d4b0cd93c8b6c560
claudio966 <claudiomatheus966@gmail.com>
2025-10-29 17:26:27 -0300

    feat: Adding support to generate datasets with jitter as the target label

--- traffic_generator/generate_data.py
+++ traffic_generator/generate_data.py
@@ -1,8 +1,8 @@
-'''
+"""
 Generate deep learning input-output 
 dataset from traffic generator data
 Created by Cláudio Modesto
-'''
+"""
 
 import json
 import glob
@@ -12,20 +12,20 @@ import numpy as np
 import networkx as nx
 import tensorflow as tf
 
-def _get_network_data(experiment_path, exp_file):
-    '''
+def _get_network_data(experiment_path, exp_file, target):
+    """
     Generate input-output data from data generator data
-    '''
-    with open(f'{experiment_path}/mininet_data.json', 'r', encoding='utf-8') as f:
-        flow_paths = json.load(f)['paths']
-    with open(f'{experiment_path}/mininet_data.json', 'r', encoding='utf-8') as f:
-        packet_size = json.load(f)['tr_metadata']['tr_metadata']['packet_size']
+    """
+    with open(f"{experiment_path}/mininet_data.json", "r", encoding="utf-8") as f:
+        flow_paths = json.load(f)["paths"]
+    with open(f"{experiment_path}/mininet_data.json", "r", encoding="utf-8") as f:
+        packet_size = json.load(f)["tr_metadata"]["tr_metadata"]["packet_size"]
     # load topology used in experiment
-    topology = nx.read_gml(f'{experiment_path}/topology.gml')
+    topology = nx.read_gml(f"{experiment_path}/topology.gml")
 
     flows = []
-    exp_conn_id = int(exp_file.split('/')[-1].split('_')[0])
-    with open(exp_file, "r", encoding='utf-8') as file:
+    exp_conn_id = int(exp_file.split("/")[-1].split("_")[0])
+    with open(exp_file, "r", encoding="utf-8") as file:
         for line in file:
             numbers = line.rstrip("\n").split(" ")  # Extract all numbers
             if float(numbers[1]) == 0 or float(numbers[4]) < 0:
@@ -42,27 +42,30 @@ def _get_network_data(experiment_path, exp_file):
                                     [1, 4, 3, -2, -1, 2], axis=1)
         filtered_flow_param[:, 0] = filtered_flow_param[:, 0] # convert to bits/s
 
+    # removing nan samples from jitter features
+    filtered_flow_param = filtered_flow_param[~np.isnan(filtered_flow_param[:, 2])]
+
     link_to_flow = []
     # get the links used for each flows
     for conn_id in filtered_flow_param[:, -2]:
         link_to_flow.append([topology.get_edge_data(
-                        str(node[0]), str(node[1]))[0].get('id')
+                        str(node[0]), str(node[1]))[0].get("id")
                     for node in flow_paths[str(int(conn_id))]])
 
     propag_delay = []
     # get propagation delay for each link
     for conn_id in filtered_flow_param[:, -2]:
         propag_delay.append([topology.get_edge_data(
-                        str(node[0]), str(node[1]))[0].get('delay')
+                        str(node[0]), str(node[1]))[0].get("delay")
                     for node in flow_paths[str(int(conn_id))]])
     # get propagation delay per-flow
     propag_delay = np.sum(propag_delay, axis=1) / 10e3 # convert to s
-    delay_budget = np.full(len(propag_delay), 
+    delay_budget = np.full(len(propag_delay),
                         np.mean(filtered_flow_param[:, -1]) * 0.4 + np.mean(filtered_flow_param[:, -1]))
 
     # get capacity of used links
     capacity = [topology.get_edge_data(
-                        str(node[0]), str(node[1]))[0].get('capacity')
+                        str(node[0]), str(node[1]))[0].get("capacity")
                     for node in flow_paths[str(int(exp_conn_id))]]
     capacity = np.array(capacity) * 10e3 # convert to kbits/s
     # get the flows used for each links
@@ -110,66 +113,111 @@ def _get_network_data(experiment_path, exp_file):
     for _ in range(batch_size, aux_flow_to_link.shape[1] + 1, batch_size):
         flow_to_link.append([[[j, 0] for j in range(batch_size)] * len(capacity)])
     flow_to_link = np.array(flow_to_link)
-    print(flow_to_link)
     flow_to_link = flow_to_link.reshape(flow_to_link.shape[0],
                                         len(capacity),
                                         batch_size,
                                         flow_to_link.shape[-1])
     samples = []
-    for i in range(filtered_flow_param.shape[0]):
-        if any(x == 0 for x in filtered_flow_param[i, :, -1] * 1000):
-            continue
-        sample = (
-            {
-                # identifier features
-                "flow_traffic": np.expand_dims(
-                    filtered_flow_param[i, :, 0], axis=1
-                ),
-                "flow_loss_packet": np.expand_dims(
-                    filtered_flow_param[i, :, 1], axis=1
-                ),
-                "jitter": np.expand_dims(
-                    filtered_flow_param[i, :, 2], axis=1
-                ),
-                "flow_packet_size": np.expand_dims(
-                    filtered_flow_param[i, :, 3], axis=1
-                ),
-                "flow_propag_delay": np.expand_dims(
-                    propag_delay[i, :], axis=1
-                ),
-                "flow_delay_budget": np.expand_dims(
-                    delay_budget[i, :], axis=1
-                ),
 
-                "flow_length": np.expand_dims(
-                    [len(flow) for flow in link_to_flow[i]], axis=1),
-                # link attributes
-                "link_capacity": np.expand_dims(
-                    capacity, axis=1
-                ),
-                # topology attributes
-                "link_to_flow": np.array(link_to_flow[i]),
-                "flow_to_link": np.array(flow_to_link[i]),
-            },
-            filtered_flow_param[i, :, -1], # flow delay
-        )
-        samples.append(sample)
+    if target == "delay":
+        for i in range(filtered_flow_param.shape[0]):
+            if any(x == 0 for x in filtered_flow_param[i, :, -1] * 1000):
+                continue
+            sample = (
+                {
+                    # identifier features
+                    "flow_traffic": np.expand_dims(
+                        filtered_flow_param[i, :, 0], axis=1
+                    ),
+                    "flow_loss_packet": np.expand_dims(
+                        filtered_flow_param[i, :, 1], axis=1
+                    ),
+                    "jitter": np.expand_dims(
+                        filtered_flow_param[i, :, 2], axis=1
+                    ),
+                    "flow_packet_size": np.expand_dims(
+                        filtered_flow_param[i, :, 3], axis=1
+                    ),
+                    "flow_propag_delay": np.expand_dims(
+                        propag_delay[i, :], axis=1
+                    ),
+                    "flow_delay_budget": np.expand_dims(
+                        delay_budget[i, :], axis=1
+                    ),
+
+                    "flow_length": np.expand_dims(
+                        [len(flow) for flow in link_to_flow[i]], axis=1),
+                    # link attributes
+                    "link_capacity": np.expand_dims(
+                        capacity, axis=1
+                    ),
+                    # topology attributes
+                    "link_to_flow": np.array(link_to_flow[i]),
+                    "flow_to_link": np.array(flow_to_link[i]),
+                },
+                filtered_flow_param[i, :, -1], # per-flow delay in seconds
+            )
+            samples.append(sample)
+
+    elif target == "jitter":
+        for i in range(filtered_flow_param.shape[0]):
+            if any(x == 0 for x in filtered_flow_param[i, :, 2] * 1000):
+                continue
+            sample = (
+                {
+                    # identifier features
+                    "flow_traffic": np.expand_dims(
+                        filtered_flow_param[i, :, 0], axis=1
+                    ),
+                    "flow_loss_packet": np.expand_dims(
+                        filtered_flow_param[i, :, 1], axis=1
+                    ),
+                    "jitter": np.expand_dims(
+                        filtered_flow_param[i, :, 2], axis=1
+                    ),
+                    "flow_packet_size": np.expand_dims(
+                        filtered_flow_param[i, :, 3], axis=1
+                    ),
+                    "flow_propag_delay": np.expand_dims(
+                        propag_delay[i, :], axis=1
+                    ),
+                    "flow_delay_budget": np.expand_dims(
+                        delay_budget[i, :], axis=1
+                    ),
+
+                    "flow_length": np.expand_dims(
+                        [len(flow) for flow in link_to_flow[i]], axis=1),
+                    # link attributes
+                    "link_capacity": np.expand_dims(
+                        capacity, axis=1
+                    ),
+                    # topology attributes
+                    "link_to_flow": np.array(link_to_flow[i]),
+                    "flow_to_link": np.array(flow_to_link[i]),
+                },
+                filtered_flow_param[i, :, 2] * 1000, # per-flow jitter in ms
+            )
+            samples.append(sample)
+
+    else:
+        raise ValueError("Choose a valid target QoS metric!")
 
     return samples
 
-def _generator(experiment_path):
-    experiment_path = experiment_path.decode('utf-8')
-    for exp_file in glob.glob(f'{experiment_path}/*_metric_results.txt'):
+def _generator(experiment_path, target):
+    experiment_path = experiment_path.decode("utf-8")
+    target = target.decode("utf-8")
+    for exp_file in glob.glob(f"{experiment_path}/*_metric_results.txt"):
         print(exp_file)
-        samples = _get_network_data(experiment_path, exp_file)
+        samples = _get_network_data(experiment_path, exp_file, target)
         for i, _ in enumerate(samples):
             yield samples[i]
 
 
-def generate_tf_data(experiment_path):
-    '''
+def generate_tf_data(experiment_path, target):
+    """
     Convert numpy data to tensorflow data structure
-    '''
+    """
     signature = (
         {
             "flow_traffic": tf.TensorSpec(shape=(None, 1), dtype=tf.float32),
@@ -188,7 +236,7 @@ def generate_tf_data(experiment_path):
 
     ds = tf.data.Dataset.from_generator(
         _generator,
-        args=[experiment_path],
+        args=[experiment_path, target],
         output_signature=signature,
     )
 
@@ -196,9 +244,9 @@ def generate_tf_data(experiment_path):
 
     return ds
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run the traffic generator")
-    parser.add_argument("--exp-path",
+    parser.add_argument("--input-dir",
                         type=str,
                         required=True,
                         help="Path to experiment files")
@@ -206,17 +254,22 @@ if __name__ == '__main__':
                         type=str,
                         required=True,
                         help="Path to output directory")
+    parser.add_argument("--target",
+                        type=str,
+                        required=True,
+                        help="Type of QoS metric to be target label")
     args = parser.parse_args()
 
     # saving tensorflow input-output data
     tf.data.Dataset.save(
     generate_tf_data(
-        args.exp_path,
+        args.input_dir,
+        args.target
     ),
     args.output_dir,
     compression="GZIP",
     )
-    N_FOLDS = 30 # 20
+    N_FOLDS = 20 # used to divide trainig and testing datasets 
     # Split dataset into n folds
     ds = tf.data.Dataset.load(args.output_dir, compression="GZIP")
     ds_split = [ds.shard(N_FOLDS, ii) for ii in range(N_FOLDS)]
