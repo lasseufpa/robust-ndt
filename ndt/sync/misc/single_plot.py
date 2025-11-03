c1595dd6a87646d0051752324df94dc46d23c25d
claudio966 <claudiomatheus966@gmail.com>
2025-11-03 11:35:30 -0300

    feat: adding support for jitter plots

--- ndt/sync/misc/single_plot.py
+++ ndt/sync/misc/single_plot.py
@@ -1,9 +1,11 @@
 """
 Script to create paper single plot (NMSE)
 Created by Cláudio Modesto
+LASSE
 """
 
 import os
+import argparse
 import pathlib
 import numpy as np
 from matplotlib import pyplot as plt
@@ -14,12 +16,25 @@ OUTPUT_PATH_NAME = "figures"
 if not os.path.isdir(OUTPUT_PATH_NAME):
     pathlib.Path(OUTPUT_PATH_NAME).mkdir(parents=True, exist_ok=True)
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--topology", "-t", help="Type of topology to be used in the experiments.", 
+    type=str, required=True
+)
+
+parser.add_argument(
+    "--target", "-g", help="Type of QoS predicted.", 
+    type=str, required=True
+)
+
+args = parser.parse_args()
+
 all_cd = [[68, 131, 194], [72, 143, 211], [72, 135, 198]]
 nmse_w_drift = np.array([])
 
 # load data from all npz file
 for i in range(10):
-    with open(f"../results/5g_crosshaul/results_sync_jitter_True_r_{i}.npz", "rb") as f:
+    with open(f"../results/{args.topology}/results_sync_{args.target}_True_r_{i}.npz", "rb") as f:
         if nmse_w_drift.size == 0:
             nmse_w_drift = np.load(f)["arr_0"]
         else:
@@ -28,7 +43,7 @@ for i in range(10):
             drift_detected = np.load(f)["arr_1"]
             model_updated = np.load(f)["arr_2"]
 
-with open("../results/5g_crosshaul/results_sync_jitter_False_r_0.npz", "rb") as f:
+with open(f"../results/{args.topology}/results_sync_{args.target}_False_r_0.npz", "rb") as f:
     nmse_wo_drift = np.load(f)["arr_0"]
 
 avg_nmse_drift = np.mean(nmse_w_drift, axis=0)
@@ -56,10 +71,20 @@ for i in range(len(all_cd[0]) - 1):
     plt.axvline(x=all_cd[0][i], color="g", linestyle="dotted")
 plt.axvline(x=all_cd[0][-1], color="g", linestyle="dotted", label="Actual concept drift")
 
+title_name = ""
+if args.topology == "5g_crosshaul":
+    title_name = "5G-Crosshaul"
+elif args.topology == "germany":
+    title_name = "Germany"
+elif args.topology == "passion":
+    title_name = "PASSION"
+
+
 plt.legend(loc="upper left")
 plt.tight_layout()
+plt.title(f"{args.target.title()} prediction performance in {title_name}", fontsize=15)
 plt.xlabel("Window index", fontsize=15)
 plt.ylabel("Average NMSE (dB)", fontsize=15)
 plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
-plt.savefig("figures/single_plot_nmse.pdf", bbox_inches="tight")
+plt.savefig(f"figures/single_plot_nmse_{args.topology}_{args.target}.pdf", bbox_inches="tight")
